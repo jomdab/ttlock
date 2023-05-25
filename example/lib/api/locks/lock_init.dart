2d63327c71256a9e49bd75b4f59fd6770cefcca7
gravitumn <namezaa.20111@gmail.com>
2023-05-25 10:56:05 +0700

    add lock init function

--- example/lib/api/locks/lock_init.dart
+++ example/lib/api/locks/lock_init.dart
@@ -1,21 +1,25 @@
+import 'dart:convert';
+
 import 'package:ttlock_flutter_example/api/api_config.dart';
 import 'package:http/http.dart' as http;
+import 'package:ttlock_flutter_example/api/locks/get_lock_list.dart';
+import 'package:ttlock_flutter_example/user.dart';
 
 Future<String> lockInit(String lockData) async {
   var url = Uri.parse('https://euapi.ttlock.com/v3/lock/initialize');
-
   var body = {
-    'client_id': APIConfig.clientId,
-    'accessToken': APIConfig.clientSecret,
+    'clientId': APIConfig.clientId,
+    'accessToken': APIConfig.accessToken,
     'lockData': lockData,
-    'date': APIConfig.currentTime,
+    'date': APIConfig.currentTime.toString(),
   };
 
   var res = await http.post(url, body: body);
   if (res.statusCode != 200)
-    throw Exception('http.get error: statusCode= ${res.statusCode}');
+    throw Exception('http.get error: statusCode5= ${res.statusCode}');
   else {
     print(res.body);
+    User.locklist = jsonDecode(await getLockList())['list'];
     return res.body;
   }
 }
